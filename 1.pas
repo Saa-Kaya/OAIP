d9a59b55bd424171315426aab665370c20a007b1
FistingAss300bucks <117829265+FistingAss300bucks@users.noreply.github.com>
2022-11-09 15:39:37 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,13 @@
+﻿var summa: integer;
+a:array[1..7] of integer;
+begin
+  var n:=7;
+for var i:=1 to n do
+ begin
+ readln(a[i]);
+ summa +=a[i];
+  end;
+  var k:= ReadInteger('Введите к: ');
+  if summa>k then print('Yes')
+  else print('No');
+end.
